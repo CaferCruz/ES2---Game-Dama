c2587c8779b958cd3de3053be726c0ab8d7807e8
Lucasboden <lucashippertt@id.uff.br>
2016-11-30 10:57:31 -0200

    Classes Jogador, Peca e Tabuleiro prontas.

---
+++ Model/Jogador.py
@@ -0,0 +1,17 @@
+from Peca import *
+class Jogador(object):
+    def __init__(self, pecas, nome):
+        self.pecas = pecas
+        self.nome  = nome
+    def __init__(self, pecas):
+        self.pecas = pecas
+    def moverPecas(self, tabuleiro, peca, posDestino):
+        if peca.cor == 0:
+            tabuleiro.lista_das_brancas.remove(peca)
+            tabuleiro.lista_das_brancas.append(Peca(peca.cor, posDestino))
+        if peca.cor == 1:
+            tabuleiro.lista_das_pretas.remove(peca)
+            tabuleiro.lista_das_pretas.append(Peca(peca.cor, posDestino))
+
+
+
