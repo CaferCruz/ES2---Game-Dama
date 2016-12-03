7d58ce74f94786ff0665b3bfbb378d70fad056a0
brunobolandini <bruno.svp@gmail.com>
2016-12-03 21:33:05 -0200

    Inicio da logica minmax: comer e comer multiplas vezes

---
+++ Model/LogicaMinmax.py
@@ -0,0 +1,7 @@
+Primeiro calcular se vai comer
+Se coordenada esta dentro do limite do tabuleiro
+Se tem alguma peca na coordenada
+    se a peca e inimiga
+    se tem espaco atras da peca
+        Come, chama esse procedimento recursivamente pra ver se pode comer mias de uma vez
+
